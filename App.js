b574f1918bc1f6f795fbf167671abd50b44646e2
gadkari <gadkari@umich.edu>
2018-10-13 23:18:58 -0400

    Added the form

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 //import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, Text, View, AppRegistry } from "react-native";
+import { StyleSheet, Text, View, AppRegistry, Button } from "react-native";
 import { MapView } from "expo";
 import { createBottomTabNavigator } from 'react-navigation'
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import t from 'tcomb-form-native';
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -21,6 +23,17 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const settings = { timestampsInSnapshots: true};
 const db = firebaseApp.firestore()
 db.settings(settings)
+
+
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  Name: t.String,
+  Location: t.String,
+  "How long will you study?": t.String,
+  //terms: t.Boolean
+});
 
 class MapScreen extends React.Component {
 	constructor(props) {
@@ -93,10 +106,22 @@ class MapScreen extends React.Component {
 }
 
 class SettingsScreen extends React.Component {
+  handleSubmit = () => {
+    const value = this._form.getValue(); // use that ref to get the form value
+    console.log('value: ', value);
+  }
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
+      <View style={styles.container}>
+        <Form 
+          ref={c => this._form = c} // assign a ref
+          type={User} 
+        />
+        <Button
+          title="Confirm"
+          onPress={this.handleSubmit}
+        />
       </View>
     );
   }
@@ -136,4 +161,27 @@ const styles = StyleSheet.create({
       fontSize: 15,
     },
 
+    container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+
 });
+
+/*const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+});*/
