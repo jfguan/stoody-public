53a85e15c31a3ccf4f0059fdaa4d084cab011556
alexewu <alexewu@umich.edu>
2018-11-15 12:30:20 -0500

    redesigned button

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Ionicons as Icon } from '@expo/vector-icons';
 //import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, Text, View, AppRegistry, Button, Platform} from "react-native";
+import { StyleSheet, Text, View, AppRegistry, Button, Platform, TouchableHighlight} from "react-native";
 import { MapView, Constants, Location, Permissions} from "expo";
 import { createBottomTabNavigator } from 'react-navigation'
 import * as firebase from 'firebase';
@@ -26,16 +26,242 @@ const db = firebaseApp.firestore()
 db.settings(settings)
 
 
+//=====================STYLING STUFF(WORK IN PROGRESS)=====================
+var LABEL_COLOR = "#000000";
+var INPUT_COLOR = "#000000";
+var ERROR_COLOR = "#a94442";
+var HELP_COLOR = "#999999";
+var BORDER_COLOR = "#cccccc";
+var DISABLED_COLOR = "#777777";
+var DISABLED_BACKGROUND_COLOR = "#eeeeee";
+var FONT_SIZE = 17;
+var FONT_WEIGHT = "500";
+
+var stylesheet = Object.freeze({
+  fieldset: {},
+  // the style applied to the container of all inputs
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+    error: {
+      marginBottom: 10
+    }
+  },
+  controlLabel: {
+    normal: {
+      color: LABEL_COLOR,
+      fontSize: FONT_SIZE,
+      marginBottom: 7,
+      fontWeight: FONT_WEIGHT
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: ERROR_COLOR,
+      fontSize: FONT_SIZE,
+      marginBottom: 7,
+      fontWeight: FONT_WEIGHT
+    }
+  },
+  helpBlock: {
+    normal: {
+      color: HELP_COLOR,
+      fontSize: FONT_SIZE,
+      marginBottom: 2
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: HELP_COLOR,
+      fontSize: FONT_SIZE,
+      marginBottom: 2
+    }
+  },
+  errorBlock: {
+    fontSize: FONT_SIZE,
+    marginBottom: 2,
+    color: ERROR_COLOR
+  },
+  textboxView: {
+    normal: {},
+    error: {},
+    notEditable: {}
+  },
+  textbox: {
+    normal: {
+      fontFamily: "Times New Roman",
+      color: INPUT_COLOR,
+      fontSize: FONT_SIZE,
+      height: 36,
+      paddingVertical: Platform.OS === "ios" ? 7 : 0,
+      paddingHorizontal: 7,
+      borderRadius: 4,
+      borderColor: BORDER_COLOR,
+      borderWidth: 1,
+      marginBottom: 5
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: INPUT_COLOR,
+      fontSize: FONT_SIZE,
+      height: 36,
+      paddingVertical: Platform.OS === "ios" ? 7 : 0,
+      paddingHorizontal: 7,
+      borderRadius: 4,
+      borderColor: ERROR_COLOR,
+      borderWidth: 1,
+      marginBottom: 5
+    },
+    // the style applied when the textbox is not editable
+    notEditable: {
+      fontSize: FONT_SIZE,
+      height: 36,
+      paddingVertical: Platform.OS === "ios" ? 7 : 0,
+      paddingHorizontal: 7,
+      borderRadius: 4,
+      borderColor: BORDER_COLOR,
+      borderWidth: 1,
+      marginBottom: 5,
+      color: DISABLED_COLOR,
+      backgroundColor: DISABLED_BACKGROUND_COLOR
+    }
+  },
+  checkbox: {
+    normal: {
+      marginBottom: 4
+    },
+    // the style applied when a validation error occours
+    error: {
+      marginBottom: 4
+    }
+  },
+  pickerContainer: {
+    normal: {
+      marginBottom: 4,
+      borderRadius: 4,
+      borderColor: BORDER_COLOR,
+      borderWidth: 1
+    },
+    error: {
+      marginBottom: 4,
+      borderRadius: 4,
+      borderColor: ERROR_COLOR,
+      borderWidth: 1
+    },
+    open: {
+      // Alter styles when select container is open
+    }
+  },
+  select: {
+    normal: Platform.select({
+      android: {
+        paddingLeft: 7,
+        color: INPUT_COLOR
+      },
+      ios: {}
+    }),
+    // the style applied when a validation error occours
+    error: Platform.select({
+      android: {
+        paddingLeft: 7,
+        color: ERROR_COLOR
+      },
+      ios: {}
+    })
+  },
+  pickerTouchable: {
+    normal: {
+      height: 44,
+      flexDirection: "row",
+      alignItems: "center"
+    },
+    error: {
+      height: 44,
+      flexDirection: "row",
+      alignItems: "center"
+    },
+    active: {
+      borderBottomWidth: 1,
+      borderColor: BORDER_COLOR
+    },
+    notEditable: {
+      height: 44,
+      flexDirection: "row",
+      alignItems: "center",
+      backgroundColor: DISABLED_BACKGROUND_COLOR
+    }
+  },
+  pickerValue: {
+    normal: {
+      fontSize: FONT_SIZE,
+      paddingLeft: 7
+    },
+    error: {
+      fontSize: FONT_SIZE,
+      paddingLeft: 7
+    }
+  },
+  datepicker: {
+    normal: {
+      marginBottom: 4
+    },
+    // the style applied when a validation error occours
+    error: {
+      marginBottom: 4
+    }
+  },
+  dateTouchable: {
+    normal: {},
+    error: {},
+    notEditable: {
+      backgroundColor: DISABLED_BACKGROUND_COLOR
+    }
+  },
+  dateValue: {
+    normal: {
+      color: INPUT_COLOR,
+      fontSize: FONT_SIZE,
+      padding: 7,
+      marginBottom: 5
+    },
+    error: {
+      color: ERROR_COLOR,
+      fontSize: FONT_SIZE,
+      padding: 7,
+      marginBottom: 5
+    }
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "white",
+    alignSelf: "center"
+  },
+  button: {
+    height: 36,
+    backgroundColor: "#48BBEC",
+    borderColor: "#48BBEC",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: "stretch",
+    justifyContent: "center"
+  }
+});
+
+//====================================================================
 
 const Form = t.form.Form;
+t.form.Form.stylesheet = stylesheet;
 
 const User = t.struct({
   Name: t.String,
   Location: t.String,
-  Studytime: t.String,
+  Study_Time: t.String,
   //terms: t.Boolean
 });
 
+
+//======================SCREEN CONTAINING MAP==========================
+//=====================================================================
 class MapScreen extends React.Component {
 	constructor(props) {
 		super(props);
@@ -105,6 +331,9 @@ class MapScreen extends React.Component {
   }
 }
 
+
+//======================SCREEN CONTAINING FORM=========================
+//=====================================================================
 class FormScreen extends React.Component {
   constructor(props) {
 	  super(props);
@@ -147,7 +376,7 @@ class FormScreen extends React.Component {
       		...this.state,
   			name: value.Name,
   			spec_loc: value.Location,
-  			time_study: value.Studytime,
+  			time_study: value.Study_Time,
   			stoody: false,
   			text: "Delete"
   		  }, this.start_stoody);
@@ -191,16 +420,16 @@ class FormScreen extends React.Component {
           ref={c => this._form = c} // assign a ref
           type={User} 
         />
-        <Button
-          //title="Confirm"
-          onPress={this.handleSubmit}
-          title = {this.state.text}
-        />
+        <TouchableHighlight style={styles.button} onPress={this.handleSubmit} underlayColor='#cc6899'>
+          <Text style={styles.buttonText}>Confirm</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
+//======================SCREEN CONTAINING FRIENDS==========================
+//=====================================================================
 class FriendsScreen extends React.Component {
   render() {
     return (
@@ -274,6 +503,32 @@ export default class IconExample extends React.Component {
 	}
 }
 
+//testing out styling the form(status: does nothing)
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10,
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600',
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
+  }
+}
+
+
 const styles = StyleSheet.create({
     mainFont: {
         fontFamily: 'Futura',
@@ -290,12 +545,27 @@ const styles = StyleSheet.create({
     	tintColor: 'red',
     },
     container: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
   	},
-    
+    button: {
+        height: 36,
+        backgroundColor: '#ddaad8',
+//        borderColor: '#ddaad8',
+//        borderWidth: 1,
+        borderRadius: 17,
+        marginBottom: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
+    buttonText: {
+        fontFamily: 'Futura',
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
 });
 
 
