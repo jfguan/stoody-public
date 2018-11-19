20e520f2cb75919d687ab3977fdb4a9733580288
jfguan <jfguan@umich.edu>
2018-11-19 13:18:21 -0500

    button js

--- App.js
+++ App.js
@@ -148,11 +148,6 @@ class FormScreen extends React.Component {
   }
 
   componentWillMount() {
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
       this._getLocationAsync();
     }
   }
