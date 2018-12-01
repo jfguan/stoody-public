8d8cc382a9b9c33ba98ce772cdd45bc0318360f3
jfguan <jfguan@umich.edu>
2018-12-01 18:57:36 -0500

    api key

--- loading.js
+++ loading.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import * as firebase from 'firebase';
+
 export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
