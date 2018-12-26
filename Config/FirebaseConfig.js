b0aa068c09ca288da58abc94b4e9e356af7f0454
alexewu <alexewu@umich.edu>
2018-12-26 16:34:40 -0500

    cant figure out firebase stuff

---
+++ Config/FirebaseConfig.js
@@ -0,0 +1,23 @@
+import Fb from 'firebase';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDnnSaCl_BCymYXC6T7GFx5hgGRioa2djg",
+	authDomain: "stoody-7c7e1.firebaseapp.com",
+	databaseURL: "https://stoody-7c7e1.firebaseio.com",
+	projectId: "stoody-7c7e1",
+	storageBucket: "stoody-7c7e1.appspot.com",
+	messagingSenderId: "388094586243"
+};
+
+
+
+//their stuff
+const Firebase = Fb.initializeApp(firebaseConfig);
+
+//our stuff
+//const firebaseApp = firebase.initializeApp(firebaseConfig);
+//const settings = { timestampsInSnapshots: true};
+//const db = firebaseApp.firestore()
+//db.settings(settings)
+
+export default Firebase;
