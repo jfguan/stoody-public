0fc996d8778cc5c017b37d84bce3e2c8a41112b2
jfguan <jfguan@umich.edu>
2018-12-30 14:47:45 -0500

    update map.js JSlint

--- map.js
+++ map.js
@@ -14,43 +14,42 @@ export default class MapScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = { isLoading: true, markers: [] };
-	}
-	
+    }
+
     //fetch marker data every 3 seconds
-	componentDidMount() {
-		this.timer = setInterval(()=> this.fetchMarkerData(), 3000);
-	}
-    
+    componentDidMount() {
+        this.timer = setInterval(()=> this.fetchMarkerData(), 3000);
+    }
+
     //pushes data into temp array and in .setState() sets markers as temp
     //TO-DO: figure out way to retreive autheticated user's data
-	fetchMarkerData() {
-		let temp = [];
-		var dataObject = {};
-		
-		var users = db.collection('users');
-		var query = users.get()
-		.then(snapshot => {
-			snapshot.forEach(doc => {
-				var data = doc.data();
-				var id = doc.id;
-				temp.push(doc.data())
-			});
-			this.setState({
-				isLoading: false,
-				markers: temp
-			});
-		})
+    fetchMarkerData() {
+        let temp = [];
+        var dataObject = {};
+        var users = db.collection('users');
+        var query = users.get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                var data = doc.data();
+                var id = doc.id;
+                temp.push(doc.data())
+            });
+            this.setState({
+                isLoading: false,
+                markers: temp
+            });
+        })
 
-	}
+    }
 
     //places markers on map
-	renderMarkers() {
+    renderMarkers() {
         //loops through markers array and adds each marker to the map
-		return this.state.isLoading ? null : this.state.markers.map((marker, index) => {
-			const coords = {
-				latitude: marker.g_loc._lat,
-				longitude: marker.g_loc._long
-			};
+        return this.state.isLoading ? null : this.state.markers.map((marker, index) => {
+            const coords = {
+                latitude: marker.g_loc._lat,
+                longitude: marker.g_loc._long
+            };
 
             //https://github.com/react-community/react-native-maps/blob/master/docs/marker.md
             return(
