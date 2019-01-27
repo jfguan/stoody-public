668d3af17d5ad36b740ce1c1c439c14a88d96f05
Jeff Guan <40967966+jfguan@users.noreply.github.com>
2019-01-26 21:37:58 -0500

    Update FirebaseConfig.js

--- Config/FirebaseConfig.js
+++ Config/FirebaseConfig.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDnnSaCl_BCymYXC6T7GFx5hgGRioa2djg",
-	authDomain: "stoody-7c7e1.firebaseapp.com",
-	databaseURL: "https://stoody-7c7e1.firebaseio.com",
-	projectId: "stoody-7c7e1",
-	storageBucket: "stoody-7c7e1.appspot.com",
-	messagingSenderId: "388094586243"
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: ""
 };
 
 
